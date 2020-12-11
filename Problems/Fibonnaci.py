a7928c1e33e89d8d9138ee73f45a151d34aae7df
Daniel Diaz <danieldiazgonza52@gmail.com>
2020-12-10 23:37:09 -0500

    Updated Problems

--- Problems/Fibonnaci.py
+++ Problems/Fibonnaci.py
@@ -15,9 +15,12 @@ class Fibonacci:
 
     # Max nth number allowed 1000
     def get_fibonacci(self, n_position):
-        n_position -= 1
         if 0 == n_position or 1 == n_position:
             return 1
+        elif -1 == n_position:
+            return 0
+        elif -1 > n_position:
+            return "Negative number"
         return self.get_fibonacci(n_position - 1) + self.get_fibonacci(n_position - 2)
 
 
@@ -27,7 +30,7 @@ def main():
         while n > 0:
             fibonacci_lenght = int(input("Nth Fibonacci number > "))
             my_Fibonacci = Fibonacci()
-            print(my_Fibonacci.get_fibonacci(fibonacci_lenght))
+            print(my_Fibonacci.get_fibonacci(fibonacci_lenght - 1))
             n -= 1
     except ValueError:
         print("Fibonacci must be a number")
